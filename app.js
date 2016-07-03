194253c4174b6331f3969fdb7bfe3c9bc4de6024
mkmarchan <mick.marchan@gmail.com>
2016-07-03 15:48:30 -0700

    Dynamically loading tracklist

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-angular.module('myApp', ['ngMaterial'])
- .config(function($mdThemingProvider) {
-   $mdThemingProvider.theme('default')
-     .primaryPalette('blue-grey')
-     .accentPalette('blue-grey');
- });
+var app = angular.module('myApp', ['ngMaterial'])
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('grey');
+  });
 
-// app.controller('musicController' function() {
-//   this.playlists = ;
-// })
+  app.controller('MusicController', function() {
+    function reqListener () {
+      this.trax = JSON.parse(this.responseText);
+    }
+    this.oReq = new XMLHttpRequest();
+    this.oReq.addEventListener("load", reqListener);
+    this.oReq.open("GET", "https://api.soundcloud.com/users/6425962/tracks?format=json&client_id=d9611ceddfd118f8d3c6890fa3b0a0f5");
+    this.oReq.send();
+
+    /*if (this.oReq.responseText != "undefined") {
+      console.log("hey");
+      this.oReq.trax = JSON.parse(this.responseText);
+    }*/
+  });
+
+SC.initialize({
+  client_id: 'd9611ceddfd118f8d3c6890fa3b0a0f5'
+});
+
+/*
+SC.get('/users/6425962/tracks', function(track) {
+  $('#Titles').html(track.title);
+  alert("test");
+  alert(track.title);
+});
+*/
