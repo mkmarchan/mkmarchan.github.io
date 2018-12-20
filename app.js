44c2c474efe9738fa6e89c412b58f71e22b15bd2
Mick Marchan <mick.marchan@gmail.com>
2018-12-20 00:54:43 -1000

    stuff working yo

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var app = angular.module('myApp', ['ngMaterial'])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-  .primaryPalette('blue-grey')
-  .accentPalette('grey')
-});
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue-grey')
+      .accentPalette('grey')
+  });
 
 app.controller('WindowController', ['$scope', '$sce',
   function($scope, $sce) {
     $.getJSON("resources/projects.json", function(json) {
       $scope.projects = json.projects;
-      $scope.projects.forEach(function (project) {
+      $scope.projects.forEach(function(project) {
         if (project.hasVideo) {
           project.videoURL = $sce.trustAsResourceUrl(project.videoURL);
         }
       })
     });
-}]);
+  }
+]);
