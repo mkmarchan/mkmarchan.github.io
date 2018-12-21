87470ca45407bb718d41c8de75076c000b30871b
Mick Marchan <mick.marchan@gmail.com>
2018-12-20 20:58:37 -1000

    about me added and semi working

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ app.controller('WindowController', ['$scope', '$sce',
     });
   }
 ]);
+
+// Profile picture resizing. Assumes it is square and at least 922 pixels wide.
+// super hacky
+// TODO initial size is off for some reason
+var firstResize = true;
+var interval = window.setInterval(resizeProfilePicture, 1);
+function resizeProfilePicture() {
+  var picture = document.getElementById("profilePicture");
+  var pictureRect = picture.getBoundingClientRect();
+  var defaultLength = Math.min(922, picture.parentElement.parentElement.scrollWidth * 0.95);
+  if (firstResize && defaultLength != 0) {
+    firstResize = false;
+    window.clearInterval(interval);
+  }
+
+  var diff = document.body.scrollHeight - (pictureRect.y + defaultLength + 32);
+  if (diff < 0 && document.body.scrollWidth >= 960) {
+    picture.style.width = picture.style.height = defaultLength + diff + "px";
+  } else {
+    picture.style.width = picture.style.height = defaultLength + "px";
+  }
+}
+$(window).resize(resizeProfilePicture);
